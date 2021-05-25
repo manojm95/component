0bdbc3fc4ffdac908ab1742d7a528f0c6c72d794
Manoj Raju <manojm95@gmail.com>
2021-05-25 10:08:55 -0400

    done

--- src/index.js
+++ src/index.js
@@ -2,25 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetails';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className = "ui comments Container"> 
+        <ApprovalCard >
          <CommentDetail 
          author = 'nivi' 
          Timeago = 'yesterday at 6.00PM' 
          commentDisp ='great cooking' 
          srcimg={faker.image.avatar()}/>
+         </ApprovalCard>
+         <ApprovalCard >
          <CommentDetail 
          author='mano' 
          Timeago = 'yesterday at 6.00PM' 
          commentDisp ='love the video' 
          srcimg={faker.image.avatar()}/>
+         </ApprovalCard>
+
+         <ApprovalCard>
          <CommentDetail 
          author='aarav' 
          Timeago = 'today at 6.00PM'
          commentDisp = 'wow' 
          srcimg='faker.image.avatar()'/>
+         </ApprovalCard>
         </div>
     );
 };
